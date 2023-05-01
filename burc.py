2a33f47af639d682199baa5a44462b4a14ea91b3
Mamaklicengo <128487934+Mamaklicengo@users.noreply.github.com>
2023-05-01 14:41:34 +0300

    Update burc.py

--- burc.py
+++ burc.py
@@ -1,43 +1,47 @@
+from telethon.sync import TelegramClient
+from telethon import events
 import requests
 from bs4 import BeautifulSoup
-from telegram.ext import Updater, CommandHandler
 
-# Telegram API token'ınızı buraya girin
-token = '6061198850:AAHAVRNvVRNOv81teRsLWwghhbx4FKXUWL8'
+api_id = YOUR_API_ID
+api_hash = 'YOUR_API_HASH'
+client = TelegramClient('session_name', api_id, api_hash)
 
-# Burç yorumlarını almak için kullanacağımız web sitesinin URL'si
-url = "https://www.hurriyet.com.tr/mahmure/astroloji/"
+# Burçlar
+burclar = {
+    'Kova': 'aquarius',
+    'Balık': 'pisces',
+    'Koç': 'aries',
+    'Boğa': 'taurus',
+    'İkizler': 'gemini',
+    'Yengeç': 'cancer',
+    'Aslan': 'leo',
+    'Başak': 'virgo',
+    'Terazi': 'libra',
+    'Akrep': 'scorpio',
+    'Yay': 'sagittarius',
+    'Oğlak': 'capricorn'
+}
 
-# Burç yorumlarını çekmek için bir fonksiyon oluşturun
-def get_horoscope(sign):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    horoscope_list = soup.find_all('div', {'class': 'horoscope-box'})
-    for horoscope in horoscope_list:
-        if horoscope.find('h6').text.lower() == sign:
-            return horoscope.find('p').text.strip()
+# Günlük burç yorumları
+def get_gunluk_burc_yorumu(burc):
+    url = f'https://www.hurriyet.com.tr/mahmure/astroloji/{burclar[burc]}-burcu'
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    yorum = soup.find('div', {'class': 'lead'}).text.strip()
+    return yorum
 
-# /burc komutu için bir fonksiyon oluşturun
-def burc(update, context):
-    # Kullanıcının hangi burcun yorumunu istediğini belirleyin
-    sign = context.args[0].lower()
-
-    # Burç yorumunu alın
-    horoscope = get_horoscope(sign)
-
-    # Eğer yorum varsa, kullanıcının mesajına yanıt olarak gönderin
-    if horoscope:
-        context.bot.send_message(chat_id=update.effective_chat.id, text=horoscope)
+# Komut işleme
+@client.on(events.NewMessage(pattern='/burc'))
+async def handle_message(event):
+    # Komut parametreleri alınır
+    param = event.raw_text.split(' ')[1].capitalize()
+    # Geçerli bir burç ise günlük yorumu alınır
+    if param in burclar.keys():
+        yorum = get_gunluk_burc_yorumu(param)
+        await event.respond(f'{param} burcu günlük yorumu:\n\n{yorum}')
     else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text=f"{sign.capitalize()} burcu için bir yorum bulunamadı.")
+        await event.respond('Geçerli bir burç giriniz.')
 
-# Updater ve Dispatcher oluşturun
-updater = Updater(token, use_context=True)
-dispatcher = updater.dispatcher
-
-# /burc komutuna yanıt vermek için bir CommandHandler oluşturun
-burc_handler = CommandHandler('burc', burc)
-dispatcher.add_handler(burc_handler)
-
-# Botu başlatın
-updater.start_polling()
+client.start()
+client.run_until_disconnected()
