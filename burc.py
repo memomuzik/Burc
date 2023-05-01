9ae79d4373df9fbab5686ddcfda264df91fba594
Mamaklicengo <128487934+Mamaklicengo@users.noreply.github.com>
2023-05-01 18:43:37 +0300

    Update burc.py

--- burc.py
+++ burc.py
@@ -1,58 +1,34 @@
-import asyncio
-import logging
-import os
-import random
-
 from telethon import TelegramClient, events
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
+# Telegram API bilgilerinizi girin
+api_id = 25989627
+api_hash = 'dff2250c7620fef64cd17e4355432d82'
+bot_token = '6061198850:AAHAVRNvVRNOv81teRsLWwghhbx4FKXUWL8'
 
-# Set up logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
+# Telegram istemcisini başlat
+client = TelegramClient('burc_bot', api_id, api_hash).start(bot_token=bot_token)
 
-# Set up environment variables
-api_id = int(os.environ.get(25989627))
-api_hash = os.environ.get('dff2250c7620fef64cd17e4355432d82')
-bot_token = os.environ.get('6061198850:AAHAVRNvVRNOv81teRsLWwghhbx4FKXUWL8')
-channel_username = os.environ.get('burcyorumvefa')
-
-
-# Set up Telegram client
-client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
-
-
-# Get daily horoscope
-def get_horoscope(sign):
-    url = f'https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-{sign.lower()}.aspx'
-    html = requests.get(url).text
-    soup = BeautifulSoup(html, 'html.parser')
-    horoscope = soup.find('div', {'class': 'main-horoscope'}).p.text.strip()
+# Burç yorumlarını çeken fonksiyon
+def get_horoscope(burc):
+    url = f'https://www.hurriyet.com.tr/mahmure/astroloji/{burc}-burcu/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    horoscope = soup.find('p', class_='horoscope-text').text.strip()
     return horoscope
 
-
-# Handle "/start" command
+# Botun tepki vermesi için ayarlar
 @client.on(events.NewMessage(pattern='/start'))
-async def handle_start(event):
-    await event.respond("Hi! I'm your daily horoscope bot. What's your zodiac sign? (e.g. /horoscope Aries)")
-    raise events.StopPropagation
+async def start(event):
+    await event.respond('Merhaba! Hangi burç yorumunu öğrenmek istersin?')
 
+@client.on(events.NewMessage(pattern='/burc'))
+async def horoscope(event):
+    # Komutun ardından gelen ilk kelimeyi burç olarak alır
+    burc = event.text.split()[1].lower()
+    horoscope = get_horoscope(burc)
+    await event.respond(horoscope)
 
-# Handle "/horoscope" command
-@client.on(events.NewMessage(pattern='/horoscope\s+(\w+)'))
-async def handle_horoscope(event):
-    sign = event.pattern_match.group(1)
-    horoscope = get_horoscope(sign)
-    await client.send_message(event.chat_id, f"Here's your daily horoscope for {sign}: \n\n{horoscope}")
-
-
-# Start the client
-async def main():
-    await client.send_message(channel_username, 'Bot is now online!')
-    await client.run_until_disconnected()
-
-
-if __name__ == '__main__':
-    logger.info('Starting bot...')
-    asyncio.run(main())
+# Botu çalıştır
+client.run_until_disconnected()
