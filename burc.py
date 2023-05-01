62af878c92b705504fc1f424c4b5827fd1b339e3
Mamaklicengo <128487934+Mamaklicengo@users.noreply.github.com>
2023-05-01 22:20:09 +0300

    Update burc.py

--- burc.py
+++ burc.py
@@ -21,7 +21,7 @@ def get_horoscope(burc):
 # Telegram event handler
 @client.on(events.NewMessage(pattern='/burcyorumu'))
 async def handle(event):
-    message = event.message
+    message = event.message.message
     burc = message.split(' ')[1] # örn. /burcyorumu koc
     horoscope = get_horoscope(burc)
     await event.respond(f'{burc.capitalize()} burcu günlük yorumu:\n{horoscope}')
