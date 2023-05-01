913019a54f0d050072067c35d901ee0914e9eb3b
Mamaklicengo <128487934+Mamaklicengo@users.noreply.github.com>
2023-05-01 21:44:39 +0300

    Update burc.py

--- burc.py
+++ burc.py
@@ -7,50 +7,26 @@ api_id = 25989627
 api_hash = 'dff2250c7620fef64cd17e4355432d82'
 bot_token = '6061198850:AAHAVRNvVRNOv81teRsLWwghhbx4FKXUWL8'
 
-bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
+bot = TelegramClient('burcyorum', api_id, api_hash).start(bot_token=bot_token)
 
 
-@bot.on(events.NewMessage(pattern='/horo'))
-async def horo(event):
-    url = 'https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-today.aspx?sign={}'
-    sign = event.message.text.split()[1]
+# Burç yorumu çeken fonksiyon
+def get_horoscope(burc):
+    url = 'https://www.elle.com.tr/astroloji/gunluk-burc-yorumlari'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    horoscopes = soup.find('div', {'class': 'section daily-horoscope'})
+    horoscope = horoscopes.find('div', {'class': burc}).text.strip()
+    return horoscope
 
-    if sign == 'aries':
-        sign = 'Aries'
-    elif sign == 'taurus':
-        sign = 'Taurus'
-    elif sign == 'gemini':
-        sign = 'Gemini'
-    elif sign == 'cancer':
-        sign = 'Cancer'
-    elif sign == 'leo':
-        sign = 'Leo'
-    elif sign == 'virgo':
-        sign = 'Virgo'
-    elif sign == 'libra':
-        sign = 'Libra'
-    elif sign == 'scorpio':
-        sign = 'Scorpio'
-    elif sign == 'sagittarius':
-        sign = 'Sagittarius'
-    elif sign == 'capricorn':
-        sign = 'Capricorn'
-    elif sign == 'aquarius':
-        sign = 'Aquarius'
-    elif sign == 'pisces':
-        sign = 'Pisces'
-    else:
-        return
+# Telegram event handler
+@client.on(events.NewMessage(pattern='/burcyorumu'))
+async def handle(event):
+    message = event.message
+    burc = message.split(' ')[1] # örn. /burcyorumu aslan
+    horoscope = get_horoscope(burc)
+    await event.respond(f'{burc.capitalize()} burcu günlük yorumu:\n{horoscope}')
 
-    res = requests.get(url.format(sign.lower()))
-    soup = BeautifulSoup(res.content, 'html.parser')
-    horo = soup.find_all('div', {'class': 'block-horoscope-text f16 l20'})
-    horoscope = horo[0].find_all('p')
-    message = ''
-    for hor in horoscope:
-        message += hor.text
-        message += '\n\n'
-    await event.respond('Here is your horoscope for today, {}:\n\n{}'.format(sign.capitalize(), message))
- 
-    if __name__ == '__main__':
-        bot.run_until_disconnected()
+# İstemci başlatma
+client.start()
+client.run_until_disconnected()
