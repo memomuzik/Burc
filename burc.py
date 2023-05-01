a0d704ff23b45992b15c68013abf00ac777fe68c
Mamaklicengo <128487934+Mamaklicengo@users.noreply.github.com>
2023-05-01 20:44:19 +0300

    Update burc.py

--- burc.py
+++ burc.py
@@ -7,34 +7,51 @@ api_id = 25989627
 api_hash = 'dff2250c7620fef64cd17e4355432d82'
 bot_token = '6061198850:AAHAVRNvVRNOv81teRsLWwghhbx4FKXUWL8'
 
-# Telegram istemcisini başlat
-client = TelegramClient('burc_bot', api_id, api_hash).start(bot_token=bot_token)
+bot = TelegramClient('bot', api_id, api_hash).start(bot_token=TOKEN)
 
-# Burç yorumlarını çeken fonksiyon
-def get_horoscope(burc):
-    response = requests.get(f'https://www.mynet.com/kadin/burclar-astroloji/{burc}-burcu-gunluk-yorumu')
-    soup = BeautifulSoup(response.content, 'html.parser')
-    horoscope_text = soup.find('p', class_='horoscope-text')
-    if horoscope_text:
-        horoscope = horoscope_text.text.strip()
-        return horoscope
+
+@bot.on(events.NewMessage(pattern='/horo'))
+async def horo(event):
+    url = 'https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-today.aspx?sign={}'
+    sign = event.message.text.split()[1]
+
+    if sign == 'aries':
+        sign = 'Aries'
+    elif sign == 'taurus':
+        sign = 'Taurus'
+    elif sign == 'gemini':
+        sign = 'Gemini'
+    elif sign == 'cancer':
+        sign = 'Cancer'
+    elif sign == 'leo':
+        sign = 'Leo'
+    elif sign == 'virgo':
+        sign = 'Virgo'
+    elif sign == 'libra':
+        sign = 'Libra'
+    elif sign == 'scorpio':
+        sign = 'Scorpio'
+    elif sign == 'sagittarius':
+        sign = 'Sagittarius'
+    elif sign == 'capricorn':
+        sign = 'Capricorn'
+    elif sign == 'aquarius':
+        sign = 'Aquarius'
+    elif sign == 'pisces':
+        sign = 'Pisces'
     else:
-        return f'{burc.capitalize()} burcu için yorum bulunamadı.'
-
-# Botun tepki vermesi için ayarlar
-@client.on(events.NewMessage(pattern='/start'))
-async def start(event):
-    await event.respond('Merhaba! Hangi burç yorumunu öğrenmek istersin?')
-
-@client.on(events.NewMessage(pattern='/burc'))
-async def horoscope(event):
-    # Komutun ardından gelen kelime sayısı 2'den az ise hata mesajı gönder
-    if len(event.text.split()) < 2:
-        await event.respond('Lütfen bir burç adı girin.')
         return
-    # Komutun ardından gelen ilk kelimeyi burç olarak alır
-    burc = event.text.split()[1].lower()
-    horoscope = get_horoscope(burc)
-    await event.respond(horoscope)
-# Botu çalıştır
-client.run_until_disconnected()
+
+    res = requests.get(url.format(sign.lower()))
+    soup = BeautifulSoup(res.content, 'html.parser')
+    horo = soup.find_all('div', {'class': 'block-horoscope-text f16 l20'})
+    horoscope = horo[0].find_all('p')
+    message = ''
+    for hor in horoscope:
+        message += hor.text
+        message += '\n\n'
+    await event.respond('Here is your horoscope for today, {}:\n\n{}'.format(sign.capitalize(), message))
+
+
+if name == 'main':
+    bot.run_until_disconnected()
