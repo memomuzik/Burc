00110cc102210ef094a13de319521c5e784c2476
Mamaklicengo <128487934+Mamaklicengo@users.noreply.github.com>
2023-05-03 12:11:24 +0300

    Update burc.py

--- burc.py
+++ burc.py
@@ -1,35 +1,60 @@
-from telethon import TelegramClient, events
-import requests
-from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-# Telegram API bilgilerinizi girin
-api_id = 25989627
-api_hash = 'dff2250c7620fef64cd17e4355432d82'
-bot_token = '6061198850:AAHAVRNvVRNOv81teRsLWwghhbx4FKXUWL8'
+import telebot
 
-client = TelegramClient('burcbot', api_id, api_hash).start(bot_token=bot_token)
+# Telegram bot token'ınızı buraya girin
+bot = telebot.TeleBot("TOKEN")
 
-browser = webdriver.Chrome() # Chrome tarayıcısını aç
-browser.get("https://www.mynet.com/kadin/burclar-astroloji")
-burc_yorumlari = browser.find_elements(By.XPATH, "//div[contains(@class, 'tab-panel')]") # Burç yorumlarını al
-browser.close() # Tarayıcıyı kapat
+# Burçlar ve bağlantıları
+astrology_links = {
+    "koç": "https://www.hurriyet.com.tr/astroloji/rezzan-kiraz/gunluk-burc-yorumlari/koc",
+    "boğa": "https://www.hurriyet.com.tr/astroloji/rezzan-kiraz/gunluk-burc-yorumlari/boga",
+    "ikizler": "https://www.hurriyet.com.tr/astroloji/rezzan-kiraz/gunluk-burc-yorumlari/ikizler",
+    "yengeç": "https://www.hurriyet.com.tr/astroloji/rezzan-kiraz/gunluk-burc-yorumlari/yengec",
+    "aslan": "https://www.hurriyet.com.tr/astroloji/rezzan-kiraz/gunluk-burc-yorumlari/aslan",
+    "başak": "https://www.hurriyet.com.tr/astroloji/rezzan-kiraz/gunluk-burc-yorumlari/basak",
+    "terazi": "https://www.hurriyet.com.tr/astroloji/rezzan-kiraz/gunluk-burc-yorumlari/terazi",
+    "akrep": "https://www.hurriyet.com.tr/astroloji/rezzan-kiraz/gunluk-burc-yorumlari/akrep",
+    "yay": "https://www.hurriyet.com.tr/astroloji/rezzan-kiraz/gunluk-burc-yorumlari/yay",
+    "oğlak": "https://www.hurriyet.com.tr/astroloji/rezzan-kiraz/gunluk-burc-yorumlari/oglak",
+    "kova": "https://www.hurriyet.com.tr/astroloji/rezzan-kiraz/gunluk-burc-yorumlari/kova",
+    "balık": "https://www.hurriyet.com.tr/astroloji/rezzan-kiraz/gunluk-burc-yorumlari/balik"
+}
+
+# Burç yorumlarını alma fonksiyonu
+def get_horoscope(burc):
+   try:
+        # Tarayıcı sürücüsünü başlatın
+        driver = webdriver.Chrome()
+        driver.get(astrology_links[burc])
+
+        # Burç yorumunu alın ve düzenleyin
+        horoscope = driver.find_element_by_class_name("horoscope-detail").text.strip()
+
+        # Tarayıcı sürücüsünü kapatın
+        driver.quit()
+
+        return horoscope
+    except:
+        return "Burç yorumu alınamadı."
+
+# /start komutu için işlev
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Merhaba! Günlük burç yorumlarını almak için burcunuzu yazın.")
+
+# Burç isimlerine göre yanıt verme işlevi
+@bot.message_handler(func=lambda message: True)
+def send_horoscope(message):
+    burc = message.text.lower()
+
+    if burc in astrology_links.keys():
+        horoscope = get_horoscope(burc)
+        bot.reply_to(message, horoscope)
+    else:
+        bot.reply_to(message, "Geçersiz burç adı. Lütfen tekrar deneyin.")
+
+# Botu çalıştırın
+bot.polling()
 
 
-# Bot başlatıldığında /start komutuyla karşılama mesajı gönder
-@client.on(events.NewMessage(pattern='/start'))
-async def start(event):
-    await event.respond('Merhaba, ben burc yorum botu! Bugünün burç yorumları şunlar:\n\n')
-
-
-# Burç yorumlarını kullanıcıya göster
-@client.on(events.NewMessage(pattern='/yorumlar'))
-async def yorumlar(event):
-    for yorum in burc_yorumlari:
-        burc_adlari = yorum.find_elements(By.XPATH, "//a[contains(@class, 'tab-label')]")
-        burc_yorumu = yorum.find_element(By.XPATH, "//p[not(@class)]")
-        msg = f"{burc_adlari[0].text} burcu:\n{burc_yorumu.text}\n\n"
-        await event.respond(msg)
-
-# Botu çalıştır
-client.run_until_disconnected()
+# kodları kendi Telegram botunuzda çalıştırarak, kullanıcılardan aldığınız burç isimlerine göre, Hurriyet Astroloji sayfasından günlük burç yorumlarını alabilirsiniz. Ancak, bu kodu çalıştırmadan önce `TOKEN` yerine kendi Telegram bot token'ınızı girmeniz ve ayrıca `webdriver.Chrome()` yerine kullandığınız tarayıcının sürücüsüne göre değiştirmeniz gerekiyor.
