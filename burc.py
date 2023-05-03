630147bf73939aa9e0796db5739650133c9eceab
Mamaklicengo <128487934+Mamaklicengo@users.noreply.github.com>
2023-05-03 20:05:42 +0300

    Update burc.py

--- burc.py
+++ burc.py
@@ -1,23 +1,72 @@
+from telethon import TelegramClient, events
 import requests
 from bs4 import BeautifulSoup
-from telethon.sync import TelegramClient
+import datetime
 
-# Telegram botu API anahtarları
 api_id = '25989627'
 api_hash = 'dff2250c7620fef64cd17e4355432d82'
 bot_token = '6061198850:AAHAVRNvVRNOv81teRsLWwghhbx4FKXUWL8'
-channel_username = 'burcyorumkanal'
 
-# İnternet sitesinden metni alın ve işleyin
-url = 'https://www.mynet.com/kadin/burclar-astroloji/koc-burcu-gunluk-yorumu.html'
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
-text = soup.get_text()
+bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 
-# Telethon ile Telegram botunu başlatın
-client = TelegramClient('session_name', api_id, api_hash).start(bot_token=bot_token)
+@bot.on(events.NewMessage(pattern='/start'))
+async def start(event):
+    await event.respond('Merhaba, burç yorumlarına hoş geldiniz! Lütfen burcunuzu seçin: \nKoç ♈️\nBoğa ♉️\nİkizler ♊️\nYengeç ♋️\nAslan ♌️\nBaşak ♍️\nTerazi ♎️\nAkrep ♏️\nYay ♐️\nOğlak ♑️\nKova ♒️\nBalık ♓️')
 
-# Metni Telegram kanalına gönderin
-client.send_message(channel_username, text)
+@bot.on(events.NewMessage(pattern='/koç'))
+async def koç(event):
+    await event.respond('Burcunuz Koç ♈️\n\n' + get_horoscope('koç'))
 
-client.run_until_disconnected()
+@bot.on(events.NewMessage(pattern='/boğa'))
+async def boğa(event):
+    await event.respond('Burcunuz Boğa ♉️\n\n' + get_horoscope('boğa'))
+
+@bot.on(events.NewMessage(pattern='/ikizler'))
+async def ikizler(event):
+    await event.respond('Burcunuz İkizler ♊️\n\n' + get_horoscope('ikizler'))
+
+@bot.on(events.NewMessage(pattern='/yengeç'))
+async def yengeç(event):
+    await event.respond('Burcunuz Yengeç ♋️\n\n' + get_horoscope('yengeç'))
+
+@bot.on(events.NewMessage(pattern='/aslan'))
+async def aslan(event):
+    await event.respond('Burcunuz Aslan ♌️\n\n' + get_horoscope('aslan'))
+
+@bot.on(events.NewMessage(pattern='/başak'))
+async def başak(event):
+    await event.respond('Burcunuz Başak ♍️\n\n' + get_horoscope('başak'))
+
+@bot.on(events.NewMessage(pattern='/terazi'))
+async def terazi(event):
+    await event.respond('Burcunuz Terazi ♎️\n\n' + get_horoscope('terazi'))
+
+@bot.on(events.NewMessage(pattern='/akrep'))
+async def akrep(event):
+    await event.respond('Burcunuz Akrep ♏️\n\n' + get_horoscope('akrep'))
+
+@bot.on(events.NewMessage(pattern='/yay'))
+async def yay(event):
+    await event.respond('Burcunuz Yay ♐️\n\n' + get_horoscope('yay'))
+
+@bot.on(events.NewMessage(pattern='/oğlak'))
+async def oğlak(event):
+    await event.respond('Burcunuz Oğlak ♑️\n\n' + get_horoscope('oğlak'))
+
+@bot.on(events.NewMessage(pattern='/kova'))
+async def kova(event):
+    await event.respond('Burcunuz Kova ♒️\n\n' + get_horoscope('kova'))
+
+@bot.on(events.NewMessage(pattern='/balık'))
+async def balık(event):
+    await event.respond('Burcunuz Balık ♓️\n\n' + get_horoscope('balık'))
+
+def get_horoscope(burc):
+    url = 'https://www.sabah.com.tr/astroloji/' + burc + '-burcu'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    horoscope = soup.find(class_='detail-content-box')
+    date = datetime.date.today().strftime('%d.%m.%Y')
+    return f'{date} tarihli {burc.capitalize()} burcu yorumu:\n\n{horoscope.get_text()}'
+
+bot.run_until_disconnected()
