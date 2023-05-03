6d49d8958e099afe8e5f1022a0efcbe27835a1a4
Mamaklicengo <128487934+Mamaklicengo@users.noreply.github.com>
2023-05-03 20:41:46 +0300

    Update burc.py

--- burc.py
+++ burc.py
@@ -62,7 +62,7 @@ async def balık(event):
     await event.respond('Burcunuz Balık ♓️\n\n' + get_horoscope('balık'))
 
 def get_horoscope(burc):
-    url = 'https://www.mynet.com/kadin/burclar-astroloji/'+ burc + '-burcu-gunluk-yorumu'
+    url = 'http://www.astroloji.org/yildizfali/' + burc + '.asp'
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     horoscope = soup.find(class_='detail-content-box')
