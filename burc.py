36e5641194002a6dc7514164ce6ee1afe84a29bf
Mamaklicengo <128487934+Mamaklicengo@users.noreply.github.com>
2023-05-04 18:23:55 +0300

    Update burc.py

--- burc.py
+++ burc.py
@@ -73,17 +73,16 @@ def get_horoscope(burc):
     selected_text = '\n'.join(selected_lines)
     return f'{date} tarihli {burc.capitalize()} burcu yorumu:\n\n{selected_text}'
    # return f'{date} tarihli {burc.capitalize()} burcu yorumu:\n\n{horoscope.get_text()}'
-@bot.on(events.NewMessage(chats=events.ChatType.GROUP))
-async def handle_new_message(event):
-    if event.chat.title == 'your_group_name':
-        message = event.message.message
-        if message.startswith('/arkadas'):
-            chat = await bot.get_entity(event.chat_id)
-            users = await bot.get_participants(chat)
-            random_users = random.sample(users, 2)
-            for user in random_users:
-                await bot.send_message(chat, f'@{user.username} artık arkadaşınız!')
 
+@bot.on(events.NewMessage(pattern='/ask'))
+async def handle_new_message(event):
+    message = event.message.message
+    if message.startswith('!arkadas'):
+        chat = await bot.get_entity(event.chat_id)
+        users = await bot.get_participants(chat)
+        random_users = random.sample(users, 2)
+        for user in random_users:
+            await bot.send_message(chat, f'@{user.username} artık arkadaşınız!')
 
 
 bot.run_until_disconnected()
